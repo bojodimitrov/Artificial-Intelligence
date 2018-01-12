981c9d2d932594b8b996740b7b68d401c8d64a37
ENDAVA\bdimitrov <bozhidar.dimitrov@endava.com>
2018-01-12 11:39:23 +0200

    Full implementation of kMeans.

--- k_means.py
+++ k_means.py
@@ -1,24 +1,41 @@
+"""
+Implementation of kMeans algorithm
+"""
 import random
 import math
+import sys
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
-FILE = open("data/normal.txt", mode="r")
+PATH_TO_FILE = sys.argv[1]
+NUMBER_OF_CLUSTERS = sys.argv[2]
 
-content = FILE.readlines()
-DATASET = []
-for x in content:
-    pair = x.strip().split("\t")
-    DATASET.append((float(pair[0]), float(pair[1])))
+def read_data(filename):
+    """
+    Reads pairs of numbers from file and creates array of tupples
+    File: xx.xxx yy.yyy -> [(xx.xxx, yy.yyy)]
+    """
+    file = open(filename, mode="r")
+    content = file.readlines()
+    dataset = []
+    for line in content:
+        pair = line.strip().split("\t")
+        dataset.append((float(pair[0]), float(pair[1])))
+    return dataset
 
-def plot_dict(dict):
-    for key, value in dict.items():
+def plot_dict(dictionary):
+    """
+    Plots dictionary, containing centers with corresponding points in cluster
+    """
+    for key, value in dictionary.items():
         plt.scatter([i[0] for i in value], [i[1] for i in value])
+    for key in dictionary.keys():
         plt.scatter(key[0], key[1], marker="*")
-    plt.show()
 
 def calculate_cluster_points(dataset, cluster_centers):
+    """
+    Assigns points to closest cluster center
+    """
     clusters = {}
     for data in dataset:
         optimal_centroid_distance = math.inf
@@ -32,13 +49,18 @@ def calculate_cluster_points(dataset, cluster_centers):
             clusters[optimal_centroid].append(data)
         except KeyError:
             clusters[optimal_centroid] = [data]
-    
     return clusters
 
 def converged(cluster_centers, next_cluster_centers):
+    """
+    Checks if the iterations has converged
+    """
     return set(cluster_centers) == set(next_cluster_centers)
 
-def reevaluate_centers(cluster_centers, clusters):
+def reevaluate_centers(clusters):
+    """
+    Moves the center of a cluster closer to its geometrical center
+    """
     new_cluster_centers = []
     cluster_points = sorted(clusters.keys())
     for point in cluster_points:
@@ -47,6 +69,9 @@ def reevaluate_centers(cluster_centers, clusters):
     return new_cluster_centers
 
 def find_centers(dataset, number_of_clusters):
+    """
+    The core of the algorithm
+    """
     cluster_centers = random.sample(dataset, number_of_clusters)
     next_cluster_centers = random.sample(dataset, number_of_clusters)
     while np.array_equal(cluster_centers, next_cluster_centers):
@@ -55,14 +80,15 @@ def find_centers(dataset, number_of_clusters):
     while not converged(cluster_centers, next_cluster_centers):
         cluster_centers = next_cluster_centers
         new_clusters = calculate_cluster_points(dataset, next_cluster_centers)
-        next_cluster_centers = reevaluate_centers(cluster_centers, new_clusters)
-        plot_dict(new_clusters)
+        next_cluster_centers = reevaluate_centers(new_clusters)
     return new_clusters
 
+DATASET = read_data(PATH_TO_FILE)
+
 plt.scatter([i[0] for i in DATASET], [i[1] for i in DATASET])
-plt.show()
+#plt.savefig("normal_scattered.png")
 
 RESULT = find_centers(DATASET, 4)
 plot_dict(RESULT)
 
-plt.show()
+plt.savefig("clustered.png")
